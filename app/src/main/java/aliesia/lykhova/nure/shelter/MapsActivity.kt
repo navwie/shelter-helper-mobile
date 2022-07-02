4402ef43e2e85441f58189465e25399df57e01d6
lykhova <navwie@gmail.com>
2022-07-02 23:18:04 +0300

    search in map

--- app/src/main/java/aliesia/lykhova/nure/shelter/MapsActivity.kt
+++ app/src/main/java/aliesia/lykhova/nure/shelter/MapsActivity.kt
@@ -2,24 +2,25 @@ package aliesia.lykhova.nure.shelter
 
 import aliesia.lykhova.nure.shelter.api.objects.GetAllShelters
 import aliesia.lykhova.nure.shelter.data.Shelter
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 import aliesia.lykhova.nure.shelter.google.CustomInfoWindowForGoogleMap
+import android.content.Intent
+import android.location.Address
+import android.location.Geocoder
+import android.os.Bundle
+import androidx.appcompat.widget.SearchView
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
-import aliesia.lykhova.nure.shelter.databinding.ActivityMapsBinding
-import android.Manifest
-import android.content.Intent
-import android.content.pm.PackageManager
-import androidx.core.app.ActivityCompat
 import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.io.IOException
+
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -28,6 +29,52 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
+
+        val search = findViewById<SearchView>(R.id.mapSearchView)
+
+        search.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                // on below line we are getting the
+                // location name from search view.
+                val location: String = search.getQuery().toString()
+
+                // below line is to create a list of address
+                // where we will store the list of all address.
+                var addressList: List<Address>? = null
+
+                // checking if the entered location is null or not.
+                if (location != null || location == "") {
+                    // on below line we are creating and initializing a geo coder.
+                    val geocoder = Geocoder(this@MapsActivity)
+                    try {
+                        // on below line we are getting location from the
+                        // location name and adding that location to address list.
+                        addressList = geocoder.getFromLocationName(location, 1)
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                    }
+                    // on below line we are getting the location
+                    // from our list a first position.
+                    val address: Address = addressList!![0]
+
+                    // on below line we are creating a variable for our location
+                    // where we will add our locations latitude and longitude.
+                    val latLng = LatLng(address.getLatitude(), address.getLongitude())
+
+                    // on below line we are adding marker to that position.
+                    mMap.addMarker(MarkerOptions().position(latLng).title(location))
+
+                    // below line is to animate camera to that position.
+                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10f))
+                }
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                return false
+            }
+        })
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as? SupportMapFragment
         mapFragment?.getMapAsync(this)
